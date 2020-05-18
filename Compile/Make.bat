3364e4e5c4b48cb560a60b23135c4206628b85a0
z-h-sun <zehaosun97@gmail.com>
2020-05-18 15:31:29 -0400

    switch to PyQt5 backend

--- Compile/Make.bat
+++ Compile/Make.bat
@@ -1,1 +1,2 @@
-pyinstaller -F --version-file=v.txt --icon=1.ico ../FETAnal.py
+set PATH=C:\mingw64\bin;%PATH%
+nuitka --mingw64 --show-progress --show-memory --show-modules --standalone --recurse-all --recurse-not-to=numpy,matplotlib --windows-icon="%~dp0\1.ico" ../FETAnal
